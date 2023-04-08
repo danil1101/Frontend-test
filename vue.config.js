7201fb17e5cb0316a23e72a9b637a2520122c81e
Danil Morozov <danilka0662@gmail.com>
2023-04-08 12:35:31 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/REPO_NAME/'
-  : '/',
   transpileDependencies: true,
 });
