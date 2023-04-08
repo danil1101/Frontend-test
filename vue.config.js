ba1416ab516a6216163821f82df144990366eb8f
Danil Morozov <danilka0662@gmail.com>
2023-04-08 10:28:26 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/Frontend-test/" : "/",
+  publicPath: "/Frontend-test/",
   transpileDependencies: true,
 });
