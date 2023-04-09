b39ef7bc01e24a09c8606e657ccce27f648bf0ff
Danil Morozov <danilka0662@gmail.com>
2023-04-09 14:40:23 +0700

    prod

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  baseURL: '/Frontend-test',
+  publicPath: '/Frontend-test',
   transpileDependencies: true,
 });
